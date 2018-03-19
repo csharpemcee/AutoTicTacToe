4dcb7051a2fa407ad47d038f03a92de9e7c2a91d
Christopher Sacchi <christophersacchi@gmail.com>
2018-03-19 10:21:35 -0400

    First step - rules - improvement
    now it always has a draw because there's no game yet and after a certain amount (I think 9) of rounds it is a draw without a win from either side

--- tictactoe.py
+++ tictactoe.py
@@ -14,27 +14,25 @@ done = False
 i = a = 0
 
 while True:
-	while done == False:
-#		rules (1 / 1 + math.exp(-rules[]))
+        done = False
 
-		if (table[0][0] == 1 and table[1][1] == 1 and table[2][2] == 1) or (table[2][0] == 1 and table[1][1] == 1 and table[0][2] == 1) or (table[0][0] == 1 and table[1][0] == 1 and table[2][0] == 1) or (table[0][1] == 1 and table[1][1] == 1 and table[2][1] == 1) or (table[0][2] == 1 and table[1][2] == 1 and table[2][2] == 1) or (table[0][0] == 1 and table[0][1] == 1 and table[0][2] == 1) or (table[1][0] == 1 and table[1][1] == 1 and table[1][2] == 1) or (table[2][0] == 1 and table[2][1] == 1 and table[2][2] == 1):
-			scores[0] += 1
-			print("X WINS")
-			break
-		elif (table[0][0] == 2 and table[1][1] == 2 and table[2][2] == 2) or (table[2][0] == 2 and table[1][1] == 2 and table[0][2] == 2) or (table[0][0] == 2 and table[1][0] == 2 and table[2][0] == 2) or (table[0][1] == 2 and table[1][1] == 2 and table[2][1] == 2) or (table[0][2] == 2 and table[1][2] == 2 and table[2][2] == 2) or (table[0][0] == 2 and table[0][1] == 2 and table[0][2] == 2) or (table[1][0] == 2 and table[1][1] == 2 and table[1][2] == 2) or (table[2][0] == 2 and table[2][1] == 2 and table[2][2] == 2):
-			scores[1] += 1
-			print("O WINS")
-			break
+        while done == False:
+#       	rules (1 / 1 + math.exp(-rules[]))
 
-		done = True
-
-		for i in range(3):
-			for a in range(3):
-				if table[i][a] == 0:
-					done = False
-			if i == 2 and done == True:
-				print("DRAW") # what we should expect a lot more of at the very end
-				break
-		i = a = 0
-	done = False
-	table = {0: {0: 0, 1: 0, 2: 0}, 1: {0: 0, 1: 0, 2: 0}, 2: {0: 0, 1: 0, 2: 0}}
+            for i in range(9):
+                    if (table[0][0] == 1 and table[1][1] == 1 and table[2][2] == 1) or (table[2][0] == 1 and table[1][1] == 1 and table[0][2] == 1) or (table[0][0] == 1 and table[1][0] == 1 and table[2][0] == 1) or (table[0][1] == 1 and table[1][1] == 1 and table[2][1] == 1) or (table[0][2] == 1 and table[1][2] == 1 and table[2][2] == 1) or (table[0][0] == 1 and table[0][1] == 1 and table[0][2] == 1) or (table[1][0] == 1 and table[1][1] == 1 and table[1][2] == 1) or (table[2][0] == 1 and table[2][1] == 1 and table[2][2] == 1):
+                        scores[0] += 1
+                        print("X WINS")
+                        done = True
+                        break
+                    elif (table[0][0] == 2 and table[1][1] == 2 and table[2][2] == 2) or (table[2][0] == 2 and table[1][1] == 2 and table[0][2] == 2) or (table[0][0] == 2 and table[1][0] == 2 and table[2][0] == 2) or (table[0][1] == 2 and table[1][1] == 2 and table[2][1] == 2) or (table[0][2] == 2 and table[1][2] == 2 and table[2][2] == 2) or (table[0][0] == 2 and table[0][1] == 2 and table[0][2] == 2) or (table[1][0] == 2 and table[1][1] == 2 and table[1][2] == 2) or (table[2][0] == 2 and table[2][1] == 2 and table[2][2] == 2):
+                        scores[1] += 1
+                        print("O WINS")
+                        done = True
+                        break
+                    elif i == 8:
+                        print("DRAW") # what we should expect a lot more of at the very end
+                        done = True
+                        break
+            i = 0
+        table = {0: {0: 0, 1: 0, 2: 0}, 1: {0: 0, 1: 0, 2: 0}, 2: {0: 0, 1: 0, 2: 0}}
